2bfd92c985a45c1eb0a6786061287e48227cdb41
harshil88 <86819569+harshil88@users.noreply.github.com>
2021-07-21 22:53:17 +0530

    Update and rename MovieGuessTwo.java to MovieGuess.java

--- src/MovieGuess.java
+++ src/MovieGuess.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 import java.io.File;
 
 
-public class MovieGuessTwo {
+public class MovieGuess {
 
     public static void main(String args[]) throws FileNotFoundException {
 
